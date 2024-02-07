d79e04357a9ca82bbbb7c24bac392e1d86e748ee
Hitesh Modi <hitesh_12014132@nitkkr.ac.in>
2024-02-08 00:48:32 +0530

    fix: parse

--- Repository/insertDatabaseRequest.js
+++ Repository/insertDatabaseRequest.js
@@ -1,10 +1,13 @@
 const client = require('../connection')
 exports.insertUserRequest = (parameters) => {
-
+  parameters[2] = parameters[2].map((val) => {
+    if (isNaN(val)) {
+      return val.replaceAll("'", `"`);
+    } else return val;
+  });
   var query = `INSERT INTO ${parameters[0]}
   (${parameters[1].join(', ')})
   VALUES ('${parameters[2].join("', '")}')`;
-
   client.query(query, (err, result) => {
     if (err) {
       throw err;
